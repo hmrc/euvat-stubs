18b2ce0ef0da1ff0a0acd80a7e272577cf6077ed
onders86 <ondersezgin+github@gmail.com>
2026-07-15 12:26:14 +0100

    DTR-6312 : F8 - Duplicate Draft Application Per Member State Check

--- app/uk/gov/hmrc/euvatstubs/controllers/EuVatCandeDataController.scala
+++ app/uk/gov/hmrc/euvatstubs/controllers/EuVatCandeDataController.scala
@@ -24,6 +24,7 @@ import uk.gov.hmrc.euvatstubs.models.{LatestApplication, LatestApplicationRespon
 import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
 
 import java.time.LocalDateTime
+import scala.util.control.NonFatal
 import javax.inject.{Inject, Singleton}
 
 @Singleton
@@ -64,30 +65,87 @@ class EuVatCandeDataController @Inject() (cc: ControllerComponents) extends Back
     logger.info("Stub: returning latest Applications")
 
     val body = request.body.asJson
+    def mapVrnToSim(v: String): String = v match {
+      case "111111115" => "SIM-5XX"
+      case "111111111" => "SIM-DUP"
+      case "222222222" => "SIM-OK"
+      case "333333333" => "SIM-EMPTY"
+      case "444444444" => "SIM-4XX"
+      case other       => other
+    }
 
-    val response = body.flatMap { json =>
-      (json \ "applicantVatRegNumber").asOpt[String].filter(_.nonEmpty) match {
-        case None => None // will trigger BadRequest
-        case Some(_) =>
-          for {
-            country   <- (json \ "refundingCountry").asOpt[String]
-            startDate <- (json \ "startDate").asOpt[String]
-            endDate   <- (json \ "endDate").asOpt[String]
-          } yield {
-            val reqStart = LocalDateTime.parse(startDate.take(19))
-            val reqEnd = LocalDateTime.parse(endDate.take(19))
-            val stubApps = latestApplicationResponse().applications.filter { app =>
-              app.refundingCountryCode == country &&
-              !app.periodStartDate.isAfter(reqEnd) &&
-              !app.periodEndDate.isBefore(reqStart)
+    val responseOpt = body.flatMap { json =>
+      // accept applicantVatRegNumber as string or numeric
+      val applicantOpt: Option[String] = (json \ "applicantVatRegNumber")
+        .asOpt[String]
+        .orElse((json \ "applicantVatRegNumber").asOpt[Long].map(_.toString))
+
+      applicantOpt.map { applicant =>
+        val sim = mapVrnToSim(applicant)
+        logger.info(s"Stub: applicantVatRegNumber=$applicant mappedTo=$sim")
+
+        sim match {
+          case "SIM-5XX"   => Left(InternalServerError("simulated 5xx"))
+          case "SIM-4XX"   => Left(BadRequest("simulated 4xx"))
+          case "SIM-EMPTY" => Right(LatestApplicationResponse(Nil, 0))
+          case "SIM-DUP"   =>
+            // return a draft application (applicationStatus = D) which should trigger duplicate validation
+            val app = LatestApplication(
+              applicationId        = 1L,
+              refundingCountryCode = (json \ "refundingCountry").asOpt[String].getOrElse("LV"),
+              periodStartDate      = LocalDateTime.of(2025, 1, 1, 0, 0),
+              periodEndDate        = LocalDateTime.of(2025, 12, 31, 23, 59),
+              applicationNumber    = "GB-DUP-0001",
+              applicationStatus    = Some("D"),
+              submissionStatus     = None, // null submissionStatus to exercise null-path as well
+              applicationVersion   = LocalDateTime.now()
+            )
+            Right(LatestApplicationResponse(List(app), 1))
+          case "SIM-OK" =>
+            val app = LatestApplication(
+              applicationId        = 2L,
+              refundingCountryCode = (json \ "refundingCountry").asOpt[String].getOrElse("LV"),
+              periodStartDate      = LocalDateTime.of(2025, 1, 1, 0, 0),
+              periodEndDate        = LocalDateTime.of(2025, 12, 31, 23, 59),
+              applicationNumber    = "GB-OK-0001",
+              applicationStatus    = Some("A"),
+              submissionStatus     = Some("S"),
+              applicationVersion   = LocalDateTime.now()
+            )
+            Right(LatestApplicationResponse(List(app), 1))
+          case _ =>
+            // default behaviour: if dates provided, filter by period; otherwise return default response
+            val countryOpt = (json \ "refundingCountry").asOpt[String]
+            val startDateOpt = (json \ "startDate").asOpt[String]
+            val endDateOpt = (json \ "endDate").asOpt[String]
+
+            val baseApps = latestApplicationResponse().applications
+
+            val filtered = (countryOpt, startDateOpt, endDateOpt) match {
+              case (Some(country), Some(sd), Some(ed)) if sd.nonEmpty && ed.nonEmpty =>
+                try {
+                  val reqStart = LocalDateTime.parse(sd.take(19))
+                  val reqEnd = LocalDateTime.parse(ed.take(19))
+                  baseApps.filter { app =>
+                    app.refundingCountryCode == country &&
+                    !app.periodStartDate.isAfter(reqEnd) &&
+                    !app.periodEndDate.isBefore(reqStart)
+                  }
+                } catch {
+                  case NonFatal(_) => baseApps
+                }
+              case (Some(country), _, _) => baseApps.filter(_.refundingCountryCode == country)
+              case _                     => baseApps
             }
-            LatestApplicationResponse(stubApps, stubApps.size)
-          }
+
+            Right(LatestApplicationResponse(filtered, filtered.size))
+        }
       }
     }
 
-    response match {
-      case Some(r) => Ok(Json.toJson(r))
-      case None    => BadRequest("applicantVatRegNumber is missing or empty")
+    responseOpt match {
+      case Some(Left(err)) => err
+      case Some(Right(r))  => Ok(Json.toJson(r))
+      case None            => BadRequest("applicantVatRegNumber is missing or empty")
     }
   }
