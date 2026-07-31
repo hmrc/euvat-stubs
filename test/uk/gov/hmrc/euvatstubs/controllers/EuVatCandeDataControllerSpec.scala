7af048ea20677551a9a71a1703a87acbc4b4b9f9
Dorothea87 <147050615+Dorothea87@users.noreply.github.com>
2026-07-31 15:01:48 +0100

    DTR-7222 add lost tests

--- test/uk/gov/hmrc/euvatstubs/controllers/EuVatCandeDataControllerSpec.scala
+++ test/uk/gov/hmrc/euvatstubs/controllers/EuVatCandeDataControllerSpec.scala
@@ -147,4 +147,31 @@ class EuVatCandeDataControllerSpec extends PlaySpec with GuiceOneAppPerSuite {
     }
   }
 
+  "EuVatCandeDataController.addPurchase" should {
+
+    def requestWith(applicationId: Long) =
+      FakeRequest("POST", "/add-purchase")
+        .withJsonBody(Json.obj("applicationId" -> applicationId, "goodsDescriptionCategory" -> "1"))
+        .withHeaders("Content-Type" -> "application/json")
+
+    "return an item number and update sequence number for a valid request" in {
+      val result = controller.addPurchase()(requestWith(123456))
+
+      status(result) mustBe OK
+      val json = contentAsJson(result)
+      (json \ "itemNumber").as[Int] mustBe 4
+      (json \ "updateSequenceNumber").as[Int] mustBe 1
+    }
+
+    "return 400 when the request body is invalid" in {
+      val fakeRequest = FakeRequest("POST", "/add-purchase")
+        .withJsonBody(Json.obj("invalid" -> "body"))
+        .withHeaders("Content-Type" -> "application/json")
+
+      val result = controller.addPurchase()(fakeRequest)
+
+      status(result) mustBe BAD_REQUEST
+    }
+  }
+
 }
