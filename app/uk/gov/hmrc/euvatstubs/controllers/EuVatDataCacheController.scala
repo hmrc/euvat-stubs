5a3ef4eaeb98161f82e83bb67e6934bb0c529b31
desi-arabadzhieva <280463992+desi-arabadzhieva@users.noreply.github.com>
2026-08-25 18:57:36 +0100

    DTR-7282 add stub data for end date warning page

--- app/uk/gov/hmrc/euvatstubs/controllers/EuVatDataCacheController.scala
+++ app/uk/gov/hmrc/euvatstubs/controllers/EuVatDataCacheController.scala
@@ -58,6 +58,8 @@ class EuVatDataCacheController @Inject() (cc: ControllerComponents) extends Back
       knownFactsResponse(vrn, "8888", deRegDate = Some(LocalDateTime.of(2025, 8, 31, 23, 59, 59, 999999999)))
     } else if (vrn.endsWith("222")) {
       knownFactsResponse(vrn, "2222", Some(LocalDateTime.of(2024, 1, 1, 0, 0, 0, 0)), Some(LocalDateTime.of(2024, 12, 31, 23, 59, 59, 999999999)))
+    } else if (vrn.endsWith("777")) {
+      knownFactsResponse(vrn, "7777", Some(LocalDateTime.of(2020, 1, 1, 0, 0, 0, 0)), Some(LocalDateTime.of(2099, 12, 31, 23, 59, 59, 999999999)))
     } else {
       knownFactsResponse(vrn, "7020", Some(LocalDateTime.of(2025, 1, 1, 0, 0, 0, 0)), Some(LocalDateTime.of(2025, 12, 31, 23, 59, 59, 999999999)))
     }
